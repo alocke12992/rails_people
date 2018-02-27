5831fc67bba90769b7bf0fb858bbbbd441ecc0da
Andrew Locke <alocke12992@gmail.com>
2018-02-27 16:04:13 -0700

    adds edit feature

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -21,6 +21,20 @@ class PeopleController < ApplicationController
     end
   end 
 
+  def edit 
+    @person = Person.find(params[:id])
+  end 
+  
+  def update 
+    @person = Person.find(params[:id])
+
+    if @person.update(person_params)
+      redirect_to people_path
+    else
+      render :edit
+    end 
+  end 
+
   private 
 
   def person_params 
